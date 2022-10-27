9fbe42a6ed9cb743037318b94dc58e7fe80e231b
Paulo Marques <78260151+paulomarquesdev@users.noreply.github.com>
2022-10-27 04:03:37 -0300

    ADD - Versão Tablet

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,9 +1,13 @@
 const swiper = new Swiper('.swiper', {
     // speed: 400,
-    spaceBetween: 10,
+    spaceBetween: 20,
     slidesPerView: 3,
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
-    }
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
   });
